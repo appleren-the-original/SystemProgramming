4659c6e11eb29b98390daf25d46ab306e077121e
AhmetY7 <ahmetylmz95@gmail.com>
2021-01-17 23:57:04 +0300

    hw3

--- HW3/hw3.c
+++ HW3/hw3.c
@@ -7,7 +7,7 @@ static const char* rofsVersion = "2008.09.24";
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
-// #include <fuse.h>
+#include <fuse.h>
 #include "cJSON.h"
 
 char dir_list[ 256 ][ 256 ];
@@ -33,6 +33,7 @@ void parse_array(cJSON *arrayItem)
 			}
 		} else if (arrayItem->child && arrayItem->child->child) {
 			if(arrayItem->string) {
+				
 				strncat(path, arrayItem->string, strlen(arrayItem->string));
 				strncat(path, "/", 1);
 			}
@@ -61,6 +62,124 @@ void parse_array(cJSON *arrayItem)
 	memset(path, '\0', sizeof(path));
 }
 
+int isFile(const char *path) {
+	printf("isFile: %s\n", path);
+	path++;
+	
+	int i=0;
+    for(i=0; i<curr_file_idx; i++) { 
+		if (strcmp(path, files_list[i]) == 0) {
+			printf("isFile i: %d\n", i);
+			return i;
+			}
+	}
+	printf("isFile i: %d\n", -1);
+	return -1;
+}
+
+static int json_getattr(const char *path, struct stat *stbuf)
+{
+	printf("getattr: %s \n", path);
+    int res = 0;
+
+    memset(stbuf, 0, sizeof(struct stat));
+    if (strcmp(path, "/") == 0) {
+        stbuf->st_mode = S_IFDIR | 0755;
+        stbuf->st_nlink = 2;
+    }
+    else if (isFile(path) != -1) {
+		
+        stbuf->st_mode = S_IFREG | 0444;
+        stbuf->st_nlink = 1;
+        printf("getattr: %s\n", files_content[isFile(path)]);
+        stbuf->st_size = strlen(files_content[isFile(path)]); 
+    }
+    else
+        res = -ENOENT;
+
+    return res;
+}
+
+
+static int json_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+                         off_t offset, struct fuse_file_info *fi)
+{
+	printf("readdir attr: %s\n", path);
+    (void) offset;
+    (void) fi;
+
+    //if (strcmp(path, "/") != 0)
+        //return -ENOENT;
+	
+    filler(buf, ".", NULL, 0);
+    filler(buf, "..", NULL, 0);
+    
+		int i=0;
+		for(i=0; i<curr_dir_idx; i++) { 
+			filler(buf, dir_list[i], NULL, 0);
+		}
+		for(i=0; i<curr_dir_idx; i++) { 
+			filler(buf, files_list[i], NULL, 0);
+		}
+	
+
+    return 0;
+}
+
+
+static int json_open(const char *path, struct fuse_file_info *fi)
+{
+	printf("json_open attr: %s\n", path);
+	int check = -1;
+	int i=0;
+    for(i=0; i<curr_dir_idx; i++) { 
+		if (strcmp(path, dir_list[i]) == 0)
+			check = 1;
+	}
+	
+	if(check != -1)
+		return -ENOENT;
+
+    if ((fi->flags & 3) != O_RDONLY)
+        return -EACCES;
+
+    return 0;
+}
+
+static int json_read(const char *path, char *buf, size_t size, off_t offset,
+                      struct fuse_file_info *fi)
+{
+	
+    printf("jjson_read attr %s:\n", path);
+    size_t len;
+    (void) fi;
+    int check = -1;
+    int i=0;
+    for(i=0; i<curr_dir_idx; i++) { 
+		if (strcmp(path, dir_list[i]) == 0)
+			check = i;
+	}
+	
+	if(check == -1)
+		return -ENOENT;
+
+    len = strlen(files_content[check]);
+    if (offset < len) {
+        if (offset + size > len)
+            size = len - offset;
+        memcpy(buf, files_content[check] + offset, size);
+    } else
+        size = 0;
+
+    return size;
+}
+
+static struct fuse_operations json_oper = {
+    .getattr	= json_getattr,
+    .readdir	= json_readdir,
+    .open	= json_open,
+    .read	= json_read,
+};
 int main(int argc, char *argv[])
 {
 	FILE *f = NULL;
@@ -94,7 +213,8 @@ int main(int argc, char *argv[])
 		printf("files_content: %s \n\n", files_content[i]);
 	}
     free(data);
-    return 0;
+    
+    return fuse_main(argc, argv, &json_oper, NULL);
 }
 
 
